1421dde2fb7e9764112f8277a9d30d3c6f416c8c
Moises Pahati <moisespahati@gmail.com>
2023-07-13 12:36:51 +0800

    add images and data - add picture of spinaci - reuse Pizza component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,72 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const pizzaData = [
+    {
+      name: "Focaccia",
+      ingredients: "Bread with italian olive oil and rosemary",
+      price: 6,
+      photoName: "pizzas/focaccia.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Margherita",
+      ingredients: "Tomato and mozarella",
+      price: 10,
+      photoName: "pizzas/margherita.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Spinaci",
+      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+      price: 12,
+      photoName: "pizzas/spinaci.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Funghi",
+      ingredients: "Tomato, mozarella, mushrooms, and onion",
+      price: 12,
+      photoName: "pizzas/funghi.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Salamino",
+      ingredients: "Tomato, mozarella, and pepperoni",
+      price: 15,
+      photoName: "pizzas/salamino.jpg",
+      soldOut: true,
+    },
+    {
+      name: "Pizza Prosciutto",
+      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+      price: 18,
+      photoName: "pizzas/prosciutto.jpg",
+      soldOut: false,
+    },
+  ];
+  
+
 function App() {
     return (
         <div>
             <h1>Hello React!!!!</h1>
-            <h2>Hello Vercel!!!</h2> 
+            <Pizza />
+            <Pizza />
         </div>
     )
+
+}
+
+function Pizza() {
+    return (
+        <div>
+            <img src="pizzas/spinaci.jpg" alt="spinaci pizza"/>
+            <h2>{pizzaData[2].name}</h2>
+            <h3>ingredients</h3>
+            <h4>{pizzaData[2].ingredients}</h4>
+        </div>
+);
 
 }
 
