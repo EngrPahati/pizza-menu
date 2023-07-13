7b5c111bdcb5b2e650b31c9b2a2deb760a63ec7f
Moises Pahati <moisespahati@gmail.com>
2023-07-13 21:20:29 +0800

    use ternary operator on footer and update footer conditional rendering

--- src/index.js
+++ src/index.js
@@ -124,14 +124,15 @@ function Footer() {
 
     return (
         <footer className="footer">
-            {isOpen && (
+            {isOpen ? (
             <div className="order">
                 <p>
                     We're open until {closeHour}:00. Come visit us or order online.
                 </p>
                 <button className="btn">Order</button>
             </div>    
-            )}
+            ) : <p>We're happy to welcome you between {openHour}:00 and { closeHour }:00.</p>
+            }
         </footer>
     );
 }
