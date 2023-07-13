e5f3110137168ce3d1eb107038f527253bb268e0
Moises Pahati <moisespahati@gmail.com>
2023-07-13 13:16:10 +0800

    add more components

--- src/index.js
+++ src/index.js
@@ -50,13 +50,34 @@ const pizzaData = [
 function App() {
     return (
         <div>
-            <h1>Hello React!!!!</h1>
-            <Pizza />
-            <Pizza />
+            <Header />
+            <Menu />
+            <Footer />
         </div>
     )
 
 }
+
+function Header() {
+    return <h1>Fast React Pizza Co.</h1>
+}
+
+function Menu() {
+    return (
+        <div>
+            <h2>Our Menu</h2>
+            <Pizza />
+            <Pizza />
+            <Pizza />
+         </div>
+     )
+}
+
+function Footer() {
+    return <footer>{ new Date().toLocaleTimeString()} We're currently open!!!</footer>
+}
+ 
+
 
 function Pizza() {
     return (
