2a49cec5c063a445d1e3f6e79070c794f888560e
Moises Pahati <moisespahati@gmail.com>
2023-07-13 21:53:20 +0800

    Create Order Component

--- src/index.js
+++ src/index.js
@@ -96,6 +96,9 @@ function Menu() {
 }
 
 function Pizza(props) {
+    
+    if (props.pizzaObj.soldOut) return null;
+
     return (
         <li className="pizza">
             <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
@@ -113,27 +116,27 @@ function Footer() {
     const openHour = 12;
     const closeHour = 22;
     const isOpen = hour >= openHour && hour <= closeHour;
-    // console.log(isOpen);
 
-
-    // if (hour >= openHour && hour <= closeHour) {
-    //     alert("We're currently open!") 
-    // } else {
-    //     alert("Sorry we're closed")
-    // }
 
     return (
         <footer className="footer">
             {isOpen ? (
-            <div className="order">
-                <p>
-                    We're open until {closeHour}:00. Come visit us or order online.
-                </p>
-                <button className="btn">Order</button>
-            </div>    
+                <Order
+                    closeHour={closeHour} />
             ) : <p>We're happy to welcome you between {openHour}:00 and { closeHour }:00.</p>
             }
         </footer>
+    );
+}
+
+function Order(props) {
+    return (
+        <div className="order">
+            <p>
+                We're open until {props.closeHour}:00. Come visit us or order online.
+            </p>
+            <button className="btn">Order</button>
+        </div>
     );
 }
  
