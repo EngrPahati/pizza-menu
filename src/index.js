62b86e572fc47672e8b260bc08f498fd0063e391
Moises Pahati <moisespahati@gmail.com>
2023-07-13 13:25:49 +0800

    add JS logic in component

--- src/index.js
+++ src/index.js
@@ -74,7 +74,20 @@ function Menu() {
 }
 
 function Footer() {
-    return <footer>{ new Date().toLocaleTimeString()} We're currently open!!!</footer>
+    const hour = new Date().getHours();
+    const openHour = 12;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour;
+    console.log(isOpen);
+
+
+    // if (hour >= openHour && hour <= closeHour) {
+    //     alert("We're currently open!") 
+    // } else {
+    //     alert("Sorry we're closed")
+    // }
+
+    return <footer>{new Date().toLocaleTimeString()} We're currently open!!!</footer>
 }
  
 
