d7d7a2071f95a8c835b59c721bbd615aefbdf385
Moises Pahati <moisespahati@gmail.com>
2023-07-13 20:00:35 +0800

    render pizza list

--- src/index.js
+++ src/index.js
@@ -78,7 +78,16 @@ function Menu() {
     return (
         <main className="menu">
             <h2>Our Menu</h2>
-            <Pizza
+            <ul className="pizzas">
+                {pizzaData.map(pizza => (
+                    <Pizza
+                        pizzaObj={pizza}
+                        key={pizza.name}
+                    />))}
+            </ul>
+            
+      
+            {/* <Pizza
                 name="Pizza Spinaci"
                 ingredients="Tomato, mozarella, spinach, and ricotta cheese"
                 photoName="pizzas/spinaci.jpg"
@@ -89,22 +98,21 @@ function Menu() {
                 ingredients="Tomato, mushrooms"
                 photoName="pizzas/funghi.jpg"
                 price={12}
-            />
+            /> */}
          </main>
      )
 }
 
 function Pizza(props) {
-    console.log(props);
     return (
-        <div className="pizza">
-            <img src={props.photoName} alt={props.name} />
+        <li className="pizza">
+            <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
             <div>
-                <h3>{ props.name }</h3>
-                <p>{props.ingredients}</p>
-                <span>${ props.price + 3}</span>
+                <h3>{ props.pizzaObj.name }</h3>
+                <p>{props.pizzaObj.ingredients}</p>
+                <span>${ props.pizzaObj.price + 3}</span>
             </div>
-        </div>
+        </li>
     );
 }
 
@@ -113,7 +121,7 @@ function Footer() {
     const openHour = 12;
     const closeHour = 22;
     const isOpen = hour >= openHour && hour <= closeHour;
-    console.log(isOpen);
+    // console.log(isOpen);
 
 
     // if (hour >= openHour && hour <= closeHour) {
